c51aef96ad0b9f9fb96f75c24f94c5b4e1ef04af
hanlinhe <646167331@qq.com>
2022-06-11 20:58:36 +0800

    提交缓存文件

--- Samples/Input System/1.0.2/Simple Demo/SimpleControls.cs
+++ Samples/Input System/1.0.2/Simple Demo/SimpleControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Samples/Input System/1.0.2/Simple Demo/SimpleControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Plugins/UnityPackageSamples/Samples/Input System/1.0.2/Simple Demo/SimpleControls.inputactions'
 
 using System;
 using System.Collections;
